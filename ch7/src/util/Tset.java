6961a8a7213a114b11e794c56ed5534e1c6b8c83
smj <amjey@naver.com>
2023-03-17 11:02:01 +0900

    생성자와 toString 오버라이드

--- ch7/src/util/Tset.java
+++ ch7/src/util/Tset.java
@@ -5,9 +5,23 @@ public class Tset {
 	private int no;
 	private String tel;
 	
+	
+	
+	public Tset(String name, int no, String tel) {
+		super();
+		this.name = name;
+		this.no = no;
+		this.tel = tel;
+	}
+
 	public void print() {}
 	public int sum() {
 		return 0;
 		}
+	
+	@Override
+	public String toString() {
+		return "Tset [name=" + name + ", no=" + no + ", tel=" + tel + "]";
+	}
 
 }
