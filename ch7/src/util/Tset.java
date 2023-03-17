a3881d7bbe5f7791917ef1be2db2b48fe73597f2
smj <amjey@naver.com>
2023-03-17 10:53:56 +0900

    생성자

--- ch7/src/util/Tset.java
+++ ch7/src/util/Tset.java
@@ -5,10 +5,21 @@ public class Tset {
 	private int no;
 	private String tel;
 	
+	
+	
+	public Tset(String name, int no, String tel) {
+		super();
+		this.name = name;
+		this.no = no;
+		this.tel = tel;
+	}
+	
 	public void print() {}
 	public int sum() {
 		return 0;
 		}
+	
+	
 	@Override
 	public String toString() {
 		return "Tset [name=" + name + ", no=" + no + ", tel=" + tel + "]";
